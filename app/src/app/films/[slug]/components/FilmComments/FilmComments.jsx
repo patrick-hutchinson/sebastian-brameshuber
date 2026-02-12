226756bd44665ba6a405684de020f7714c91358d
Patrick Hutchinson <hutchinsonpatrick@icloud.com>
2026-02-12 10:17:55 +0000

    Update FilmComments.jsx

--- app/src/app/films/[slug]/components/FilmComments/FilmComments.jsx
+++ app/src/app/films/[slug]/components/FilmComments/FilmComments.jsx
@@ -1,3 +1,5 @@
+import { useState, useRef, useEffect } from "react";
+
 import FilmCommentItem from "./FilmCommentItem";
 
 import styles from "../../FilmPage.module.css";
@@ -5,8 +7,21 @@ import styles from "../../FilmPage.module.css";
 const FilmComments = ({ film }) => {
   if (!film.comments) return undefined;
 
+  const containerRef = useRef(null);
+  const [containerHeight, setContainerHeight] = useState(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    setContainerHeight(containerRef.current.getBoundingClientRect().height);
+  }, []);
+
   return (
-    <div className={styles.filmComments} typo="longcopy">
+    <div
+      className={styles.filmComments}
+      typo="longcopy"
+      style={{ position: "sticky", top: `calc(50% - ${containerHeight / 2}px)` }}
+    >
       {film.comments?.map((comment) => (
         <FilmCommentItem comment={comment} />
       ))}
